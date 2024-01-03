28cef99c218555074554bbba1e505c04f6834d59
qqiangwu <wqzhiep@gmail.com>
2024-01-03 23:13:05 +0800

    chore(test): add github CI

--- integration_test/test.sh
+++ integration_test/test.sh
@@ -11,4 +11,11 @@ then
     extra=$2;
 fi
 
-../build/debug/cppsafe "${srcfile}" $CPPSAFE_INC --extra-arg=-Xclang --extra-arg=-verify --extra-arg=-isystem/Library/Developer/CommandLineTools/usr/include --extra-arg=-std=c++20 --extra-arg=-Wno-dangling-gsl --extra-arg=-Wno-return-stack-address --extra-arg=-Wno-unused $extra
+if [[ -f ../build/debug/cppsafe ]];
+then
+    binary=../build/debug/cppsafe
+else
+    binary=../build/release/cppsafe
+fi
+
+$binary "${srcfile}" $CPPSAFE_INC --extra-arg=-Xclang --extra-arg=-verify --extra-arg=-isystem/Library/Developer/CommandLineTools/usr/include --extra-arg=-std=c++20 --extra-arg=-Wno-dangling-gsl --extra-arg=-Wno-return-stack-address --extra-arg=-Wno-unused $extra
