534c7ee0c8f6f3e52b9f06b481eb02785e73888f
qqiangwu <wqzhiep@gmail.com>
2024-01-05 20:45:50 +0800

    test(contract): add contract annotation tests for templates

---
+++ integration_test/contract_template.cpp
@@ -0,0 +1,47 @@
+template <class T>
+void __lifetime_contracts(T&&);
+
+template <class T>
+void __lifetime_type_category_arg(T&&);
+
+constexpr int Global = 0;
+
+template <class T>
+int Foo(T* t [[clang::annotate("gsl::lifetime_pre", Global)]]);
+
+struct Test {
+    template <class T> int Foo(T* t [[clang::annotate("gsl::lifetime_pre", Global)]]);
+};
+
+template <class T>
+struct [[gsl::Pointer]] Ptr
+{
+    int foo(int* p [[clang::annotate("gsl::lifetime_pre", Global)]]);
+
+    template <class P>
+    [[clang::annotate("gsl::lifetime_pre", "p", Global)]]
+    int bar(P* p [[clang::annotate("gsl::lifetime_pre", Global)]]);
+};
+
+void test_contract()
+{
+    __lifetime_contracts(&Foo<double>);  // expected-warning {{pset(Pre(t)) = ((global))}}
+
+    __lifetime_contracts(&Test::Foo<double>);
+    // expected-warning@-1 {{pset(Pre(this)) = (*this)}}
+    // expected-warning@-2 {{pset(Pre(t)) = ((global))}}
+
+    Ptr<int> p;
+    __lifetime_type_category_arg(p);
+    // expected-warning@-1 {{lifetime type category is Pointer with pointee int}}
+
+    __lifetime_contracts(&Ptr<int>::foo);
+    // expected-warning@-1 {{pset(Pre(this)) = (*this)}}
+    // expected-warning@-2 {{pset(Pre(*this)) = ((null), **this)}}
+    // expected-warning@-3 {{pset(Pre(p)) = ((global))}}
+
+    __lifetime_contracts(&Ptr<int>::bar<int>);
+    // expected-warning@-1 {{pset(Pre(this)) = (*this)}}
+    // expected-warning@-2 {{pset(Pre(*this)) = ((null), **this)}}
+    // expected-warning@-3 {{pset(Pre(p)) = ((global))}}
+}
