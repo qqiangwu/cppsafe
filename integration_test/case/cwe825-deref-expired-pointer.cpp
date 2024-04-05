8fa9a773cd928ee8edb6640c0f0b67df8910e6dc
qqiangwu <wqzhiep@gmail.com>
2024-04-05 19:48:25 +0800

    fix(pset): fix cwe825, handle expired pointer caused by parameters

--- integration_test/case/cwe825-deref-expired-pointer.cpp
+++ integration_test/case/cwe825-deref-expired-pointer.cpp
@@ -1,17 +1,25 @@
+// ARGS: -Wlifetime-output
+
 #include "../feature/common.h"
 
-// TODO
-// expected-no-diagnostics
 class Foo {
   int *x;
   int &y;
   Foo(int a, int b);
+
+  void set(int a)
+  {
+    x = &a;  // expected-warning {{returning a dangling pointer as output value '(*this).x'}}
+    // expected-note@-1 {{pointee 'a' left the scope here}}
+  }
 };
 
 Foo::Foo(int a, int b) :
   x(&a),
   y(b)
-{};
+{
+}  // expected-warning {{returning a dangling pointer as output value '(*this).x'}}
+   // expected-note@-1 {{pointee 'a' left the scope here}}
 
 struct Foo2 {
     int* x;
@@ -19,5 +27,25 @@ struct Foo2 {
 
     Foo2(Owner<int> a, Owner<int> b)
         : x(a.getPtr()), y(b.get())
-    {}
+    {
+    }  // expected-warning {{returning a dangling pointer as output value '(*this).x'}}
+       // expected-note@-1 {{pointee 'a' left the scope here}}
+
+    void set(Owner<int> a)
+    {
+      x = a.getPtr();  // expected-warning {{returning a dangling pointer as output value '(*this).x'}}
+       // expected-note@-1 {{pointee 'a' left the scope here}}
+    }
 };
+
+int* get(int a)
+{
+  return &a;  // expected-warning {{returning a dangling pointer}}
+  // expected-note@-1 {{pointee 'a' left the scope here}}
+}
+
+void test_param(int a, int** p)
+{
+  *p = &a;  // expected-warning {{returning a dangling pointer as output value '*p'}}
+   // expected-note@-1 {{pointee 'a' left the scope here}}
+}
