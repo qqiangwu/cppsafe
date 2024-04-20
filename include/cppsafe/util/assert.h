6256b89bd2054c2469d00ed3e005c8525e5e1614
qqiangwu <wqzhiep@gmail.com>
2024-04-20 20:21:05 +0800

    feat(debug): use more informative assertion

--- include/cppsafe/util/assert.h
+++ include/cppsafe/util/assert.h
@@ -1,4 +1,13 @@
 #include <gsl/assert>
 
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define CPPSAFE_ASSERT(...) GSL_CONTRACT_CHECK("Assertion", __VA_ARGS__)
+#include <cstdio>
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-do-while, cppcoreguidelines-pro-type-vararg)
+#define CPPSAFE_ASSERT(e)                                                                                              \
+    do {                                                                                                               \
+        if (!(e)) [[unlikely]] {                                                                                       \
+            fprintf(stderr, "%s:%d: failed assertion `%s'\n", __FILE__, __LINE__, #e);                                 \
+            std::terminate();                                                                                          \
+        }                                                                                                              \
+    } while (false)
+// NOLINTEND(cppcoreguidelines-avoid-do-while, cppcoreguidelines-pro-type-vararg)
