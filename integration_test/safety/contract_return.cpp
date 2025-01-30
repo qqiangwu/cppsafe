a5a88ca1dbe8912026b354b7d5ed92b602caafbb
qqiangwu <wqzhiep@gmail.com>
2025-01-30 23:32:13 +0800

    fix(pset): fix nested invalidation inside *(*this).value

--- integration_test/safety/contract_return.cpp
+++ integration_test/safety/contract_return.cpp
@@ -33,3 +33,39 @@ int* get2()
     static int* y = &x;
     return y;
 }
+
+struct Dummy {
+    int m;
+};
+
+const int* get3();
+const int* get4(const Dummy& d);
+const int* get5(const Owner<int>& d);
+const int* get6(const Owner<Dummy>& d);
+const int* get7(const Owner<Pair<int, int>>& d);
+
+void test_contract_return()
+{
+    __lifetime_contracts(&get3);
+    // expected-warning@-1 {{pset(Post((return value))) = ((global), (null))}}
+
+    __lifetime_contracts(&get4);
+    // expected-warning@-1 {{pset(Pre(d)) = (*d)}}
+    // expected-warning@-2 {{pset(Pre((*d).m)) = (*d)}}
+    // expected-warning@-3 {{pset(Post((return value))) = ((null), *d)}}
+
+    __lifetime_contracts(&get5);
+    // expected-warning@-1 {{pset(Pre(d)) = (*d)}}
+    // expected-warning@-2 {{pset(Pre(*d)) = (**d)}}
+    // expected-warning@-3 {{pset(Post((return value))) = ((null), **d)}}
+
+    __lifetime_contracts(&get6);
+    // expected-warning@-1 {{pset(Pre(d)) = (*d)}}
+    // expected-warning@-2 {{pset(Pre(*d)) = (**d)}}
+    // expected-warning@-3 {{pset(Post((return value))) = ((null), **d)}}
+
+    __lifetime_contracts(&get7);
+    // expected-warning@-1 {{pset(Pre(d)) = (*d)}}
+    // expected-warning@-2 {{pset(Pre(*d)) = (**d)}}
+    // expected-warning@-3 {{pset(Post((return value))) = ((null), **d)}}
+}
